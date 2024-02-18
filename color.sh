b3a828c194544c372d080bc0393c0316842981e2
wo2ni <i_o_i_o@163.com>
2024-02-18 20:59:02 +0800

    :rocket: color

---
+++ color.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#--Color Code;
+Cls="\033[0m"       ;    White="\033[1;38m"
+Red="\033[1;31m"    ;    Green="\033[1;32m"
+Yellow="\033[1;33m" ;    Blue="\033[1;34m"
+Purple="\033[1;35m" ;    Cyan_blue="\033[1;36m"
+
+#--Cursor Hide and Show;
+Hide_cur="\033[?25l"   ;   Show_cur="\033[?25h"
