99cb354b00edf8f77367b9a21247f30324058f78
wo2ni <i_o_i_o@163.com>
2024-02-28 08:27:10 +0800

    :rocket: add coclor code

--- color.sh
+++ color.sh
@@ -8,3 +8,33 @@ Purple="\033[1;35m" ;    Cyan_blue="\033[1;36m"
 
 #--Cursor Hide and Show;
 Hide_cur="\033[?25l"   ;   Show_cur="\033[?25h"
+
+
+echoContent() {
+    case $1 in
+    # 红色
+    "red")
+        # shellcheck disable=SC2154
+        ${echoType} "\033[31m${printN}$2 \033[0m"
+        ;;
+        # 天蓝色
+    "skyBlue")
+        ${echoType} "\033[1;36m${printN}$2 \033[0m"
+        ;;
+        # 绿色
+    "green")
+        ${echoType} "\033[32m${printN}$2 \033[0m"
+        ;;
+        # 白色
+    "white")
+        ${echoType} "\033[37m${printN}$2 \033[0m"
+        ;;
+    "magenta")
+        ${echoType} "\033[31m${printN}$2 \033[0m"
+        ;;
+        # 黄色
+    "yellow")
+        ${echoType} "\033[33m${printN}$2 \033[0m"
+        ;;
+    esac
+}
