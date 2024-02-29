263239c33cb274a135e67cce07e13f14d099efb8
wo2ni <i_o_i_o@163.com>
2024-02-29 10:35:30 +0800

    :rocket: add check cpu code

---
+++ systeminfo.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+#
+
+function _os() {
+  local os=""
+  [[ -f "/etc/debian_version" ]] && source /etc/os-release && os="${ID}" && printf -- "%s" "${os}" && return
+  [[ -f "/etc/redhat-release" ]] && os="centos" && printf -- "%s" "${os}" && return
+}
+
+function _os_full() {
+  [[ -f /etc/redhat-release ]] && awk '{print ($1,$3~/^[0-9]/?$3:$4)}' /etc/redhat-release && return
+  [[ -f /etc/os-release ]] && awk -F'[= "]' '/PRETTY_NAME/{print $3,$4,$5}' /etc/os-release && return
+  [[ -f /etc/lsb-release ]] && awk -F'[="]+' '/DESCRIPTION/{print $2}' /etc/lsb-release && return
+}
+
+function _os_ver() {
+  local main_ver="$(echo $(_os_full) | grep -oE "[0-9.]+")"
+  printf -- "%s" "${main_ver%%.*}"
+}
+
+# 检查CPU提供商
+checkCPUVendor() {
+    if [[ -n $(which uname) ]]; then
+        if [[ "$(uname)" == "Linux" ]]; then
+            case "$(uname -m)" in
+            'amd64' | 'x86_64')
+                CPUVendor="linux-64"
+                CPUVendor="linux-64"
+                #                hysteriaCoreCPUVendor="hysteria-linux-amd64"
+                #                tuicCoreCPUVendor="-x86_64-unknown-linux-musl"
+                warpRegCoreCPUVendor="main-linux-amd64"
+                singBoxCoreCPUVendor="-linux-amd64"
+                ;;
+            'armv8' | 'aarch64')
+                cpuVendor="arm"
+                CPUVendor="linux-arm64-v8a"
+                CPUVendor="linux-arm64-v8a"
+                #                hysteriaCoreCPUVendor="hysteria-linux-arm64"
+                #                tuicCoreCPUVendor="-aarch64-unknown-linux-musl"
+                warpRegCoreCPUVendor="main-linux-arm64"
+                singBoxCoreCPUVendor="-linux-arm64"
+                ;;
+            *)
+                echo "  不支持此CPU架构--->"
+                exit 1
+                ;;
+            esac
+        fi
+    else
+        echoContent red "  无法识别此CPU架构，默认amd64、x86_64--->"
+        CPUVendor="linux-64"
+        CPUVendor="linux-64"
+    fi
+}
+
